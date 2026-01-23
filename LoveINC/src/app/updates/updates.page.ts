1f2ced66ad0faac1c3919a7d94ca613c95cfa4da
Jeff Denton <chiefupstart@gmail.com>
2026-01-22 16:19:01 -0800

    Updates cards

--- LoveINC/src/app/updates/updates.page.ts
+++ LoveINC/src/app/updates/updates.page.ts
@@ -1,19 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonIcon } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonButtons,
+  IonIcon,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent,
+} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { personCircleOutline } from 'ionicons/icons';
+import { personCircleOutline, calendarOutline } from 'ionicons/icons';
+
+export interface UpdateEvent {
+  id: string;
+  photoUrl: string;
+  title: string;
+  subtitle: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-updates',
   templateUrl: 'updates.page.html',
   styleUrls: ['updates.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonIcon, ExploreContainerComponent],
+  imports: [
+    CommonModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+    IonButtons,
+    IonIcon,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonCardContent,
+  ],
 })
-export class UpdatesPage {
-  constructor(private router: Router) {
-    addIcons({ personCircleOutline });
+export class UpdatesPage implements OnInit {
+  events: UpdateEvent[] = [];
+
+  constructor(
+    private router: Router,
+    private http: HttpClient
+  ) {
+    addIcons({ personCircleOutline, calendarOutline });
+  }
+
+  ngOnInit() {
+    this.loadEvents();
+  }
+
+  loadEvents() {
+    this.http.get<UpdateEvent[]>('assets/data/updates-events.json').subscribe({
+      next: (data) => {
+        this.events = data;
+      },
+      error: (err) => {
+        console.error('Error loading events:', err);
+      },
+    });
   }
 
   navigateToProfile() {
