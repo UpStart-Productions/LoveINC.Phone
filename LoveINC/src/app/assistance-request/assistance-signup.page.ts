47621c48ca1ad6fe9c671b45924690d9ad442399
Jeff Denton <chiefupstart@gmail.com>
2026-02-28 19:43:33 -0800

    SIgn up form

--- LoveINC/src/app/assistance-request/assistance-signup.page.ts
+++ LoveINC/src/app/assistance-request/assistance-signup.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import {
   IonHeader,
@@ -8,7 +10,16 @@ import {
   IonButton,
   IonButtons,
   IonBackButton,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonSelect,
+  IonSelectOption,
+  IonTextarea,
+  IonCheckbox,
 } from '@ionic/angular/standalone';
+import { OnboardingService } from '../services/onboarding.service';
 
 @Component({
   selector: 'app-assistance-signup',
@@ -16,6 +27,8 @@ import {
   styleUrls: ['./assistance-signup.page.scss'],
   standalone: true,
   imports: [
+    CommonModule,
+    FormsModule,
     IonHeader,
     IonToolbar,
     IonTitle,
@@ -23,12 +36,54 @@ import {
     IonButton,
     IonButtons,
     IonBackButton,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonSelect,
+    IonSelectOption,
+    IonTextarea,
+    IonCheckbox,
   ],
 })
-export class AssistanceSignupPage {
-  constructor(private router: Router) {}
+export class AssistanceSignupPage implements OnInit {
+  form = {
+    firstName: '',
+    lastName: '',
+    city: '',
+    phone: '',
+    email: '',
+    reason: '',
+    comments: '',
+    wantsNewsletter: false,
+  };
+
+  constructor(
+    private router: Router,
+    private onboardingService: OnboardingService
+  ) {}
+
+  ngOnInit() {
+    const data = this.onboardingService.getOnboardingData();
+    if (data?.firstName) this.form.firstName = data.firstName;
+    if (data?.lastName) this.form.lastName = data.lastName;
+    if (data?.email) this.form.email = data.email;
+    if (data?.wantsNewsletter != null) this.form.wantsNewsletter = data.wantsNewsletter;
+  }
+
+  canSubmit(): boolean {
+    return !!(
+      this.form.firstName.trim() &&
+      this.form.lastName.trim() &&
+      this.form.city &&
+      this.form.email.trim() &&
+      this.form.reason
+    );
+  }
 
   onSubmit() {
+    if (!this.canSubmit()) return;
+    // TODO: Submit to API
     this.router.navigate(['/assistance/thank-you']);
   }
 }
